db186ce1c0a131ce57a19df4dc56ea8d0d59f755
Mario <Cushymario@gmail.com>
2018-06-05 11:35:33 +0800

    格式化代码

--- Sweet/VIewModels/Cards/ContentCardViewModel.swift
+++ Sweet/VIewModels/Cards/ContentCardViewModel.swift
@@ -18,38 +18,35 @@ struct ContentCardViewModel {
     var resultAvatarURLs: [URL]?
     var resultComment: String?
     let defaultImageNameList: [String]
+    
     init(model: CardResponse) {
-        self.titleString = model.name!
-        self.contentString = model.content!
+        titleString = model.name!
+        contentString = model.content!
         if let imageList = model.contentImageList {
-            contentImages = imageList.map { return ContentImageModel(model: $0) }
+            contentImages = imageList.map { ContentImageModel(model: $0) }
         } else if let video = model.video {
-            self.videoURL = URL(string: video)!
+            videoURL = URL(string: video)
         }
-        self.cardId = model.cardId
+        cardId = model.cardId
         if let comment = model.result?.comment, comment != "" {
-            self.resultComment = comment
+            resultComment = comment
         } else if let emoji = model.result?.emoji, emoji != 0 {
-            self.resultImageName = "ResultEmoji\(emoji)"
-            if let users = model.result?.contactUserList {
-                var urls = [URL]()
-                users.forEach { (user) in
-                    urls.append(URL(string: user.avatar)!)
-                }
-                resultAvatarURLs = urls
-            }
+            resultImageName = "ResultEmoji\(emoji)"
+            resultAvatarURLs = model.result?.contactUserList.compactMap({ URL(string: $0.avatar) })
         }
-        self.defaultImageNameList = model.defaultEmojiList!.map { return "Emoji\($0.rawValue)"}
+        defaultImageNameList = model.defaultEmojiList!.map { "Emoji\($0.rawValue)"}
     }
 }
+
 struct ContentImageModel {
     let size: CGSize
     let imageURL: URL
+    
     init(model: ContentImage) {
         let scale = (UIScreen.mainWidth() - 20) / 27
         let width = scale * CGFloat(model.width)
         let height = scale * CGFloat(model.height)
-        self.size = CGSize(width: width, height: height)
-        self.imageURL = URL(string: model.url)!
+        size = CGSize(width: width, height: height)
+        imageURL = URL(string: model.url)!
     }
 }
