1e9419d32ef9184379607ee926341458d73ed14f
Mario <cushymario@gmail.com>
2018-07-03 18:57:41 +0800

    禁止创建个人对话入口
    修复已读标记错误；
    Fix #27

--- Sweet/Flows/ConversationFlow/ConversationCoordinator.swift
+++ Sweet/Flows/ConversationFlow/ConversationCoordinator.swift
@@ -31,7 +31,7 @@ final class ConversationCoordinator: BaseCoordinator, ConversationCoordinatorOup
     }
     
     override func start() {
-        Messenger.shared.markConversationAsRead(userID: user.userId)
+        Messenger.shared.markConversationAsRead(userID: buddy.userId)
         let conversation = ConversationController(user: user, buddy: buddy)
         conversation.delegate = self
         router.push(conversation)
