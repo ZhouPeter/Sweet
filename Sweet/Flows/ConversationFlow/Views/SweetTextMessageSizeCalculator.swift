08994502e9f67cab291ac6038363c67b9e3efde3
Mario <cushymario@gmail.com>
2018-07-06 15:11:53 +0800

    调整消息间距

--- Sweet/Flows/ConversationFlow/Views/SweetTextMessageSizeCalculator.swift
+++ Sweet/Flows/ConversationFlow/Views/SweetTextMessageSizeCalculator.swift
@@ -9,6 +9,12 @@
 import MessageKit
 
 final class SweetTextMessageSizeCalculator: TextMessageSizeCalculator {
+    override init(layout: MessagesCollectionViewFlowLayout?) {
+        super.init(layout: layout)
+        incomingMessagePadding = UIEdgeInsets(top: 10, left: 8, bottom: 0, right: 30)
+        outgoingMessagePadding = UIEdgeInsets(top: 10, left: 30, bottom: 0, right: 8)
+    }
+    
     override func messageContainerMaxWidth(for message: MessageType) -> CGFloat {
         return 200
     }
