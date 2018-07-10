b8e2a9ca549f5d50be0ba578d39a2c2d38b52f8a
Mario <cushymario@gmail.com>
2018-07-10 14:57:47 +0800

    修复文本小故事的文字标签不可点击的问题

--- Sweet/Flows/StoryFlow/Controllers/Text/StoryTextController.swift
+++ Sweet/Flows/StoryFlow/Controllers/Text/StoryTextController.swift
@@ -136,6 +136,9 @@ final class StoryTextController: BaseViewController, StoryTextView, StoryEditCan
         }
         var draft = StoryDraft(filename: name, storyType: .text, date: Date())
         draft.topic = topic
+        if editController.hasText || editController.topic != nil {
+            draft.contentRect = editController.boundingRect
+        }
         TaskRunner.shared.run(StoryPublishTask(storage: Storage(userID: user.userId), draft: draft))
         Defaults[.isPersonalStoryChecked] = false
         view.hero.id = "avatar"
