ad4954885dbd335ad6a45ab4b67d166190897ba3
Mario <cushymario@gmail.com>
2018-08-14 18:18:04 +0800

    修改视频播放全屏判断条件

--- Sweet/Flows/CardsFlow/Controllers/CardBaseController+CellDelegate.swift
+++ Sweet/Flows/CardsFlow/Controllers/CardBaseController+CellDelegate.swift
@@ -250,9 +250,7 @@ extension CardsBaseController: VideoCardCollectionViewCellDelegate {
     func showVideoPlayerController(playerView: SweetPlayerView, cardId: String) {
         guard let index = cards.index(where: { $0.cardId == cardId }) else { return }
         guard index == self.index else { return }
-        guard let avPlayer = playerView.avPlayer,
-            let asset = avPlayer.currentItem?.asset,
-            asset.isPlayable else { return }
+        guard let avPlayer = playerView.avPlayer, avPlayer.status == .readyToPlay else { return }
         playerView.hero.isEnabled = true
         playerView.hero.id = cards[index].video
         playerView.hero.modifiers = [.arc]
