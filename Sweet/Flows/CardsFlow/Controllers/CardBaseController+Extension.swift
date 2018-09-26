0832f6c9ea3b6286e4e32ea71e617a88d925e422
Mario <cushymario@gmail.com>
2018-09-26 15:49:59 +0800

    加入群组后主动更新

--- Sweet/Flows/CardsFlow/Controllers/CardBaseController+Extension.swift
+++ Sweet/Flows/CardsFlow/Controllers/CardBaseController+Extension.swift
@@ -265,6 +265,7 @@ extension CardsBaseController {
                         alert.addAction(UIAlertAction(title: "知道了", style: .default, handler: nil))
                         self.present(alert, animated: true, completion: nil)
                         Defaults[.isJoinGroupChat] = true
+                        Messenger.shared.loadConversations()
                     } else {
                         self.toast(message: "加入群聊成功！")
                         for (index, card) in self.cards.enumerated() where card.cardEnumType == .groupChat && card.groupId! == groupId {
